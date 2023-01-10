cdd2887730345cbdda3f02123e29cd19a1512968
Efe Samuel <efe.samuel@sanlam.com.ng>
2023-01-10 12:35:32 +0100

    cursor update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { Hero } from "./Components/Hero";
 import { Body } from "./Components/Body";
 import { Footer } from "./Components/Footer";
+import EfficientCursor from "react-efficient-cursor";
+import { Cursor } from "./Components/Cursor";
 
 function App() {
   return (
@@ -9,6 +11,9 @@ function App() {
       <Hero />
       <Body />
       <Footer />
+      <EfficientCursor>
+        <Cursor />
+      </EfficientCursor>
     </div>
   );
 }
